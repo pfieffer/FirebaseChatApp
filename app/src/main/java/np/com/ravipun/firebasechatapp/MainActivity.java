cc64ac166ebf26d272e80ff17ee43c7c2cd9a0af
pfieffer <cbpmgr@gmail.com>
2018-05-15 12:41:21 +0545

    Initial Commit

---
+++ app/src/main/java/np/com/ravipun/firebasechatapp/MainActivity.java
@@ -0,0 +1,13 @@
+package np.com.ravipun.firebasechatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
